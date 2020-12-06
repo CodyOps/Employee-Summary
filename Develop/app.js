965380db3c1adcc66270011d3d10d4284547e323
Cody Chen <codychen1@gmail.com>
2020-12-06 13:26:59 -0700

    added linked files to app.js

--- Develop/app.js
+++ Develop/app.js
@@ -5,11 +5,13 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+const userinput = require("Develop/lib/UserAnswers.js");
+const generateHTML = require("Develop/lib/makeHTML.js");
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
